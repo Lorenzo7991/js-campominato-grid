8b6a34e500617785a6bc6b16b8684c1ac5cb5020
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-11 18:19:11 +0100

    add final layout, game function, console log cell number

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,32 @@
 console.log("functions-JS-OK");
+
+const startGame = () => {
+    //* Inside fuction creating cell element
+    const createCell = cellNumber => {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.append(cellNumber);
+        return cell;
+    }
+
+    //*Changing button text when clicked
+    button.innerText = 'RESTART';
+
+    //*Resetting grid content
+    grid.innerText = '';
+
+    //* Loop generating cell and injecting on DOM
+    for (let i = 1; i <= 100; i++) {
+        //* Creating cell with counter number
+        const cell = createCell(i);
+
+        cell.addEventListener('click', (event) => {
+            console.log(event.target.innerText);
+        });
+
+        //*Injecting on grid
+        grid.appendChild(cell);
+    }
+
+
+}
--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log("script-JS-OK");
+
+//* Recovering DOM elements
+const grid = document.getElementById('grid');
+const button = document.querySelector('.btn-primary')
+
+//* Starting "startGame" function on button click
+button.addEventListener('click', startGame);
